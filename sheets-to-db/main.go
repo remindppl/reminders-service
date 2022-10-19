58b00e9821676d8607be0af0df85eff0cb21cd6a
chethan <chethan123@gmail.com>
2022-10-19 13:20:42 -0700

    Adding sample code - squash

---
+++ sheets-to-db/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	// Use a service account
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("path/to/serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+		"first": "Ada",
+		"last":  "Lovelace",
+		"born":  1815,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
+}
